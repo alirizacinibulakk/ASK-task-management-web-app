d90086638b66bc569077356ea1a7aa0e98e6812b
Ali Rıza Çinibulak <alirizacinibulak@gmail.com>
2025-02-18 12:09:08 +0300

    mobil css yapıldı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,25 +46,27 @@ export default function Header() {
   return (
     <>
       <header>
-        <img src="\images\logo.svg" alt="logo" />
-        <button className="toggle-menu" onClick={toggleMenu}>
+        <img className="header-logo" src="\images\logo.svg" alt="logo" />
+        <button className="toggle-menu-btn" onClick={toggleMenu}>
           <span>{currentBoardName}</span>
           <svg width="9" height="7" viewBox="0 0 9 7" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1 1L5 5L9 1" stroke="#635FC7" strokeWidth="2" />
           </svg>
         </button>
-        <div className="header-btn-left">
-          <button>+</button>
-          <button onClick={toggleModal}><img src="\images\three-dot.svg" alt="" /></button>
+        <div className="header-btn-right">
+          <button className="plus-btn"><img src="\images\+.svg" alt=""/></button>
+          <div className="modal-container">
+            <button className="dot-btn" onClick={toggleModal}><img src="\images\three-dot.svg" alt="" /></button>
+            {modal ? (
+              <div className="three-dot-modal">
+                <button>Edit Board</button>
+                <button>Delete Board</button>
+              </div>
+
+            ) : null}
+          </div>
         </div>
       </header>
-
-      {modal ? (
-        <div className="three-dot-modal">
-          <button>Edit Board</button>
-          <button>Delete Board</button>
-        </div>
-      ) : null}
 
       {isOpen ? (
         <div className="boards-menu-container">
@@ -72,11 +74,13 @@ export default function Header() {
             <h2>All Boards ({data.boards.length})</h2>
             <div className="boards">
               {data.boards.map(board => (
-                <button key={board.id} onClick={() => setCurrentBoardId(board.id)}><img src="\images\fluent_board.svg" alt="" /> {board.name}</button>
+                <button key={board.id} className={currentBoardId === board.id ? 'active' : ''} onClick={() => setCurrentBoardId(board.id)}>
+                  <img src="\images\fluent_board.svg" alt="" /> {board.name}
+                </button>
               ))}
             </div>
             <button><img src="\images\fluent_board.svg" alt="" /> +Create New Board</button>
-            <div className="theme">
+            <div className="theme-container">
               <label className="theme-switch">
                 <img src="\images\light-theme.svg" alt="" />
                 <input
